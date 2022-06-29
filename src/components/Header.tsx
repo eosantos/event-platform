551b7ef48806a243207385b8c1aca40c3570b330
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2022-06-29 19:10:06 -0300

    feat: create component lesson

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./Logo";
 
 export function Header() {
   return (
-    <header className="w-full py-5 flex items-center justify-center bg-gray-700">
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border border-gray-500">
       <Logo />
     </header>
   );
