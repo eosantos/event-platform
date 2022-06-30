11562a5327483197dcad3d792a463f1fa2583eb5
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2022-06-30 08:44:01 -0300

    feat: create component video and routers

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link, useParams } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -10,6 +11,8 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
+  const { slug } = useParams<{ slug: string }>();
+
   const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormatted = format(
     props.availableAt,
@@ -19,31 +22,33 @@ export function Lesson(props: LessonProps) {
     }
   );
 
+  const isActiveLesson = slug === props.slug;
+
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-              <CheckCircle size={20} weight="bold" />
+            <span className="text-sm font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
               Conteúdo liberado
             </span>
           ) : (
             <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-              <Lock size={20} weight="bold" />
+              <Lock size={20} />
               Em breve
             </span>
           )}
 
-          <span className="text-xs rounded py-[0.311rem] px-3 text-white border border-green-300 font-bold">
+          <span className="text-xs rounded py-[0.125rem] px-2 text-white border font-bold">
             {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
 
-        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+        <strong className="mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
