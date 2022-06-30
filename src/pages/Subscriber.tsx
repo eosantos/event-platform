10ed51c27c399d2d7d938d98fb067f256b5ba78a
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2022-06-30 16:50:57 -0300

    fix: correct text

--- src/pages/Subscriber.tsx
+++ src/pages/Subscriber.tsx
@@ -31,12 +31,12 @@ export function Subscriber() {
           <Logo />
 
           <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Construa uma{" "}
-            <strong className="text-blue-500">aplicação completa</strong>, do
-            zero, com <strong className="text-blue-500">React</strong>
+            Crie <strong className="text-blue-500">imagens </strong>
+            profissionais, do zero, com
+            <strong className="text-blue-500"> Photoshop CC </strong>
           </h1>
           <p className="mt-4 text-gray-200 leading-relaxed">
-            Em apenas uma semana você vai dominar na prática uma das tecnologias
+            Em apenas uma semana você vai dominar na prática uma das ferramentas
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
